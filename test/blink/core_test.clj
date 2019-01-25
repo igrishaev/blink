bbad44eb0d9a3d6fa291f0a151fe800450b331aa
Ivan Grishaev <ivan@grishaev.me>
2019-01-25 19:18:10 +0300

    more tests

--- test/blink/core_test.clj
+++ test/blink/core_test.clj
@@ -36,16 +36,28 @@
 
 
 (deftest test-thread
+
+  (is (= state ::none))
+
+  (is (state-down?))
+
   (state-start)
+
+  (let [doc (-> state var meta :doc)]
+    (is (= doc "Some docstring")))
 
   (is (thread? state))
   (is (alive? state))
+
+  (is (state-up?))
 
   (let [thread state]
 
     (state-stop)
 
     (is (= state ::none))
+
+    (is (state-down?))
 
     (thread? thread)
     (is (not (alive? thread)))))
